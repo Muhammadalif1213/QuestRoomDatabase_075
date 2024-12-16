1f971b48d07a6d1c5f0b1b5907ef22101c384f36
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-16 15:07:40 +0700

    membuat data class di DetailMhsViewModel untuk menampung data yang akan ditampilkan di ui

---
+++ app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.pertemuan9.ui.viewmodel
+
+data class DetailUitate(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
