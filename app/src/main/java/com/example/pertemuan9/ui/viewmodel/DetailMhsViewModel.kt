9763beb3a7e4fbe8be2ca1f49b48fd91fc5a20b6
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-16 15:30:04 +0700

    membuat class DetailMhsViewModel untuk menampilkan ui dari detail mahasiswa. mengambil data dari data class DetailUitate

--- app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,6 +1,53 @@
 package com.example.pertemuan9.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan9.data.entity.Mahasiswa
+import com.example.pertemuan9.repository.LocalRepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class DetailMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: LocalRepositoryMhs,
+): ViewModel(){
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUiEvent: StateFlow<DetailUitate> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map { DetailUitate(
+            detailUiEvent = it.toDetailUiEvent(),
+            isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUitate(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUitate(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = DetailUitate(
+                isLoading = true,
+            )
+        )
+}
 
 data class DetailUitate(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
