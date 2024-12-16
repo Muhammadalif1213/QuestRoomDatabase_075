a750f485f8e0687d018a6bca8f2d3c67ebf39f50
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-17 06:20:03 +0700

    menambahkan rute untuk destinasi ke home

--- app/src/main/java/com/example/pertemuan9/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pertemuan9/ui/navigation/AlamatNavigasi.kt
@@ -4,6 +4,10 @@ interface AlamatNavigasi {
     val route: String
 }
 
+object DestinsiHome : AlamatNavigasi{
+    override val route = "home"
+}
+
 object DestinasiDetail : AlamatNavigasi {
     override val route = "detail"
     const val NIM = "nim"
