9f26cdbb7756f2f08b142d39b88f50165773ff8a
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-16 16:21:45 +0700

    membuat fungsi untuk mengubah dari data mahasiswa menjadi mahasiswaUIstate

--- app/src/main/java/com/example/pertemuan9/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/ui/viewmodel/UpdateMhsViewModel.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.pertemuan9.data.entity.Mahasiswa
 import com.example.pertemuan9.repository.RepositoryMhs
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
@@ -79,3 +80,7 @@ class UpdateMhsViewModel(
         updateUitate = updateUitate.copy(snackBarMessage = null)
     }
 }
+
+fun Mahasiswa.toUIStateMhs(): MhsUIState = MhsUIState(
+    mahasiswaEvent = this.toDetailUiEvent(),
+)
