cb7a92b9b87890edc1abc6b4bc99501142b143bb
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-17 15:36:45 +0700

    membuat fungsi untuk melihat detail mahasiswa

--- app/src/main/java/com/example/pertemuan9/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/pertemuan9/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,20 +1,27 @@
 package com.example.pertemuan9.ui.view.mahasiswa
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -25,20 +32,69 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan9.data.entity.Mahasiswa
-import com.example.pertemuan9.ui.viewmodel.DetailUitate
+import com.example.pertemuan9.ui.customWidget.TopAppBar
+import com.example.pertemuan9.ui.viewmodel.DetailMhsViewModel
+import com.example.pertemuan9.ui.viewmodel.DetailUiState
+import com.example.pertemuan9.ui.viewmodel.PenyediaViewModel
 import com.example.pertemuan9.ui.viewmodel.toMahasiswaEntity
+
+@Composable
+fun DetailMhsView (
+    modifier: Modifier = Modifier,
+    viewModel: DetailMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { },
+    onEditClick: (String) -> Unit = { },
+    onDeleteClick: () -> Unit = {}
+) {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                judul = "Detail Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailUiState.value.detailUiEvent.nim)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa"
+                )
+            }
+        }
+    ){ innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
+
+        BodyDetailMhs(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                viewModel.deleteMhs()
+                onDeleteClick()
+            }
+        )
+    }
+}
 
 
 @Composable
 fun BodyDetailMhs(
     modifier: Modifier = Modifier,
-    detailUitate: DetailUitate = DetailUitate (),
+    detailUiState: DetailUiState,
     onDeleteClick: () -> Unit = {}
 ){
     var deleteConfifrmationRequired by rememberSaveable { mutableStateOf(false) }
     when{
-        detailUitate.isLoading ->{
+        detailUiState.isLoading ->{
             Box(
                 modifier = modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
@@ -47,14 +103,14 @@ fun BodyDetailMhs(
             }
         }
 
-        detailUitate.isUiEventNotEmpty -> {
+        detailUiState.isUiEventNotEmpty -> {
             Column (
                 modifier = modifier
                     .fillMaxWidth()
                     .padding(16.dp)
             ){
                 ItemDetailMhs(
-                    mahasiswa = detailUitate.detailUiEvent.toMahasiswaEntity(),
+                    mahasiswa = detailUiState.detailUiEvent.toMahasiswaEntity(),
                     modifier = Modifier
                 )
                 Spacer(modifier = Modifier.padding(8.dp))
@@ -79,7 +135,7 @@ fun BodyDetailMhs(
                 }
             }
         }
-        detailUitate.isUiEventEmpty -> {
+        detailUiState.isUiEventEmpty -> {
             Box(
                 modifier = Modifier.fillMaxWidth(),
                 contentAlignment = Alignment.Center
